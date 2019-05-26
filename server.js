c1a42236291c95749acf2ec883ed78f9dda64741
trustautoservices <trustautoservices@gmail.com>
2019-05-26 11:16:38 +0530

    updates to deploy to heroku node and npm version change

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tas'));
+app.use(express.static('./dist/tas'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/tas/index.html'));
+res.sendFile(path.join(__dirname,'/dist/tas/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
